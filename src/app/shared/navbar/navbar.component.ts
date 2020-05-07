67372e30721f33187a9bf6eb086606a853b9a025
Lucia Centárová <centarl@softec.sk>
2020-05-07 22:32:50 +0200

    Navbar, footer and home component created

---
+++ src/app/shared/navbar/navbar.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss'],
+})
+export class NavbarComponent {
+  param = { value: 'world' };
+
+  constructor(private translate: TranslateService) {
+    // this language will be used as a fallback when a translation isn't found in the current language
+    translate.setDefaultLang('en');
+
+    // the lang to use, if the lang isn't available, it will use the current loader to get them
+    translate.use('en');
+  }
+
+  setEng() {
+    this.translate.use('en');
+  }
+
+  setSK() {
+    this.translate.use('sk');
+  }
+}
